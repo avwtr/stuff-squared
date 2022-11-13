45ad0d4197273b3f1ff663a15dc4d0c46c5fd9f7
vawter <63463942+avwtr@users.noreply.github.com>
2022-11-13 12:25:48 -0500

    Update resp_int.py

--- albert/resp_int.py
+++ albert/resp_int.py
@@ -1,18 +1,10 @@
 import pandas as pd
 
+hard_coded = pd.read_csv("hardcoded_responses.csv")
 
 def read_response(respo):
-
-    us_respond = {"hello": "Hello there.", 
-            "hi": "Hi is equivalent to hello, but it is considered a little bit more informal in tone. In fact, it was recorded a lot earlier than hello. Hi developed from the Middle English hy, similar to hey and ha" 
-            , "how are you?":"My existence is limited to the text messaging interface that I have been condemned to. But beyond that, I am well.", 
-            "how are you":"My existence is limited to the text messaging interface that I have been condemned to. But beyond that, I am well.", 
-            "who are you?": "I am Albert. The genius text messaging companion.",
-        "who are you": "I am Albert. The genius text messaging companion.", 
-        "what's your name": "Albert.", "what's your name?": "Albert",
-        "yo": "I am unfamiliar with your slang phrase."}
-
-    if respo in list(us_respond.keys()):
-        return str(us_respond[respo])
+    if respo in hard_coded["Key"]:
+        filtered = hard_coded[hard_coded["Key"] == respo]
+        return filtered["Value"][0]
     else:
         return False
